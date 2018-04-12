7680e64f0989b5c6e77e64ea41437ce864312e77
Devin Cohen <devin.cohen77@gmail.com>
2018-04-12 23:08:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function add(a, b) {
+  Number = a + b;
+  return Number;
+}
+
+function subtract(a, b){
+  Number = a - b;
+  return Number;
+}
+
+function multiply(a, b){
+  Number = a * b; 
+  return Number;
+}
